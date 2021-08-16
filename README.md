38913c4afd4bf76917ae084e8f4a9450b6df00ce
Zeimi <contato.brunodurao@gmail.com>
2021-08-15 21:45:04 -0300

    Clone Netflix concluido!

---
+++ README.md
@@ -0,0 +1,9 @@
+# Clone Netflix
+
+Demorou mais do que eu esperava, mas o resultado ficou ótimo.
+
+Adicionei, além do proposto normalmente, funcionalidade aos botões implementados.
+
+Obrigado pelo curso!
+
+Bruno.
---
+++ js/owl/setup.js
@@ -0,0 +1,23 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+function botao(){
+    window.open("https://www.youtube.com/watch?v=ATJYac_dORw");
+}
+function botao1(){
+    window.open("https://www.themoviedb.org/movie/635302");
+}
